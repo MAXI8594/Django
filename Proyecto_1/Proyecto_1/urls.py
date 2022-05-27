6eea23fa65fcfd8e0a41cce871fe67951a92b66d
MAXI8594 <molivera154@alumnos.iu.edu.ar>
2022-05-27 01:26:55 -0300

    d

--- Proyecto_1/Proyecto_1/urls.py
+++ Proyecto_1/Proyecto_1/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.urls import path
 from Proyecto_1.views import saludo
 from Proyecto_1.views import DiadeHoy
+from Proyecto_1.views import nombre_persona
+from Proyecto_1.views import probandoTeamlates
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludo/', saludo),
-    path('segundavista/', saludo),
+    path('saludo/', saludo),    
     path('diadehoy/', DiadeHoy),
+    path('nombre_persona/<nombre>/', nombre_persona),
+    path('probandoTeamlates/', probandoTeamlates)
 ]
--- Proyecto_1/Proyecto_1/views.py
+++ Proyecto_1/Proyecto_1/views.py
@@ -1,13 +1,32 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 import datetime
+
 def saludo (request):
     return HttpResponse ("Hola mundoo")
-    
-def segundavista (request):
-    return HttpResponse ("Segunda vista")
+
 
 def DiadeHoy (request):
-
-    dia = datetime.datetime().now()
+    dia = datetime.datetime.now()
     texto = 'Hoy es: {}'.format(dia)
     return HttpResponse(texto)
+
+def nombre_persona(self , nombre):
+    resultado = "Mi nombre es : <br> <br> {}" .format(nombre)
+    return HttpResponse(resultado)
+
+#C:/Users/Usuario/Desktop/Django/Proyecto_1/Proyecto_1\plantilas\
+def probandoTeamlates(self):
+
+    nombre = 'Maxi'
+    apellido = 'Olivera'
+    dic = {"nombre":nombre,"apellido": apellido}
+
+    miHtml = open( 'C:\\Users\\Usuario\\Desktop\\Django\\Proyecto_1\\Proyecto_1\\plantilas\\template1.html')
+    plantilla = Template(miHtml.read())
+    miHtml.close()
+
+    miContexto = Context(dic)
+    documento = plantilla.reader(miContexto)
+
+    return HttpResponse(documento)
