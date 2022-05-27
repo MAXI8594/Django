937c6bcf4c9019b00ce7a76c171f80536100707c
MAXI8594 <molivera154@alumnos.iu.edu.ar>
2022-05-26 23:44:16 -0300

    s

--- Proyecto_1/Proyecto_1/urls.py
+++ Proyecto_1/Proyecto_1/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from Proyecto_1.views import saludo
+from Proyecto_1.views import DiadeHoy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
     path('segundavista/', saludo),
+    path('diadehoy/', DiadeHoy),
 ]
--- Proyecto_1/Proyecto_1/views.py
+++ Proyecto_1/Proyecto_1/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
-
+import datetime
 def saludo (request):
     return HttpResponse ("Hola mundoo")
     
 def segundavista (request):
     return HttpResponse ("Segunda vista")
+
+def DiadeHoy (request):
+
+    dia = datetime.datetime().now()
+    texto = 'Hoy es: {}'.format(dia)
+    return HttpResponse(texto)
